bd0ed86ce50edff5bcf734aa39f8b7d2a30a4381
wdotmathree <wdotmathree@gmail.com>
2025-07-16 00:11:12 -0600

    Added interface with IMU and filtered attitude calculations

---
+++ main/main.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+
+#include <i2c_master.hpp>
+#include <interrupt.hpp>
+#include <madgwick_filter.hpp>
+
+#define ACC_SCALE 4
+#define GYRO_SCALE 500
+#define CALIBRATION_ITS 600
+
+espp::I2cMasterBus
+	bus(espp::I2cMasterBus::Config{
+		.sda_io_num = 14,
+		.scl_io_num = 15,
+		.clk_speed = 400000,
+	});
+std::shared_ptr<espp::I2cMasterDevice<uint8_t>> imu_device;
+
+espp::MadgwickFilter filter; // Default beta = 0.1
+
+int16_t buf[8];
+int32_t bias_tmp[6] = {0};
+int16_t bias[6];
+
+bool init(void) {
+	std::error_code ec;
+	if (!bus.init(ec)) {
+		std::cerr << "Failed to initialize I2C bus: " << ec.message() << std::endl;
+		return false;
+	}
+	imu_device = bus.add_device<uint8_t>(
+		espp::I2cMasterDevice<uint8_t>::Config{
+			.device_address = 0x68,
+			.scl_speed_hz = 400000,
+		},
+		ec
+	);
+	if (ec) {
+		std::cerr << "Failed to add I2C device: " << ec.message() << std::endl;
+		return false;
+	}
+	buf[0] = 0x00;
+	buf[1] = 0x78;
+	buf[2] = 0x44;
+	buf[3] = 0x09;
+	buf[4] = 0x08;
+	buf[5] = 0x08;
+	imu_device->write_register(0x38, (uint8_t *)buf + 0, 1, ec); // Disable interrupts
+	imu_device->write_register(0x23, (uint8_t *)buf + 2, 1, ec); // Enable accelerometer and gyroscope in FIFO
+	imu_device->write_register(0x6a, (uint8_t *)buf + 4, 1, ec); // Enable and reset FIFO
+	imu_device->write_register(0x6b, (uint8_t *)buf + 6, 1, ec); // Wake up the device, disable temperature, PLL with x gyroscope
+	imu_device->write_register(0x1b, (uint8_t *)buf + 8, 1, ec); // 500dps gyroscope scale
+	imu_device->write_register(0x1c, (uint8_t *)buf + 10, 1, ec); // 4g accelerometer scale
+
+	return true;
+}
+
+extern "C" void app_main(void) {
+	std::error_code ec;
+
+	if (!init())
+		return;
+
+	std::cout << "Hello, World!" << std::endl;
+
+	// Calibrate
+	vTaskDelay(10);
+	for (int i = 0; i < CALIBRATION_ITS; i++) {
+		imu_device->read_register(0x3b, (uint8_t *)buf, 14, ec);
+		if (ec) {
+			i--;
+			continue;
+		}
+
+		for (int i = 0; i < 3; i++)
+			bias_tmp[i] += (int16_t)__bswap16(buf[i]);
+		for (int i = 3; i < 6; i++)
+			bias_tmp[i] += (int16_t)__bswap16(buf[i + 1]);
+	}
+	for (int i = 0; i < 6; i++)
+		bias[i] = bias_tmp[i] / CALIBRATION_ITS;
+
+	// Get precise time
+	uint64_t prev = esp_timer_get_time();
+	while (true) {
+		// vTaskDelay(1);
+		imu_device->read_register(0x3b, (uint8_t *)buf, 14, ec);
+		if (ec)
+			continue;
+
+		uint64_t now = esp_timer_get_time();
+
+		float az = (float)(int16_t)__bswap16(buf[0]) / (32768.0f / ACC_SCALE);
+		float ay = (float)(int16_t)__bswap16(buf[1]) / (32768.0f / ACC_SCALE);
+		float ax = -(float)(int16_t)__bswap16(buf[2]) / (32768.0f / ACC_SCALE);
+		float gz = (float)((int16_t)__bswap16(buf[4]) - bias[3]) / (32768.0f / GYRO_SCALE) * (M_PI / 180.0f);
+		float gy = (float)(int16_t)__bswap16(buf[5]) / (32768.0f / GYRO_SCALE) * (M_PI / 180.0f);
+		float gx = -(float)(int16_t)__bswap16(buf[6]) / (32768.0f / GYRO_SCALE) * (M_PI / 180.0f);
+
+		float mag = espp::fast_inv_sqrt(ax * ax + ay * ay + az * az);
+		filter.update((now - prev) / 1000000.f, ax * mag, ay * mag, az * mag, gx, gy, gz);
+
+		filter.get_euler(gx, gy, gz);
+		printf("%11.6f %11.6f %11.6f\n", gx, gy, gz);
+
+		prev = now;
+	}
+}
